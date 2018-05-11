7de2b27d1b0cc9071ac3497d03afc8417a6a9d75
Michelle Olivieri <michelleo72@gmail.com>
2018-05-11 03:07:08 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
+require_relative 'models/piglatinizer.rb'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post '/' do
+    @english = params[:user_phrase]
+    @pig_latin = PigLatinizer.new(params[:user_phrase])
+
+    erb :piglatinizer
+  end
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,33 @@
+require 'pry'
+
+class PigLatinizer
+
+  attr_reader :text
+
+  def vowel?(letter)
+    letter.match(/[aeiouAEIOU]/)
+  end
+
+  def piglatinize(text)
+    if text.include?(" ")
+      piglatin_sentence(text)
+
+    elsif vowel?(text[0])
+      "#{text}way"
+
+    elsif vowel?(text[1])
+      "#{text[1..-1]}#{text[0]}ay"
+    elsif
+      !vowel?(text[1]) && vowel?(text[2])
+      "#{text[2..-1]}#{text[0]}#{text[1]}ay"
+    elsif
+      !vowel?(text[1]) && !vowel?(text[2])
+      "#{text[3..-1]}#{text[0]}#{text[1]}#{text[2]}ay"
+    end
+  end
+
+    def piglatin_sentence(text)
+      text.split.map {|word| piglatinize(word)}.join(" ")
+    end
+
+end
